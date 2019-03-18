5123d60486d3ead43a1279a38e8b6a9c402abead
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-18 16:59:58 +0530

    Initial commit

---
+++ bootloader.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+ARIA2C_BUILD_NAME=aria2-1.34.0-linux-gnu-64bit-build1.tar.bz2
+
+echo "Downloading aria2c static build latest release from https://github.com/q3aql/aria2-static-builds"
+wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.34.0/aria2-1.34.0-linux-gnu-64bit-build1.tar.bz2
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+	echo "Cannot download aria2c latest release from https://github.com/q3aql/aria2-static-builds site"
+	exit 11 # terminate and indicate error
+fi
+echo "Downloaded aria2c static build extract and saved it as $ARIA2C_BUILD_NAME"
+
+
+
+echo "Extracting static build extract $ARIA2C_BUILD_NAME"
+tar xvjf $ARIA2C_BUILD_NAME --strip-components=1 aria2-1.34.0-linux-gnu-64bit-build1/aria2c aria2-1.34.0-linux-gnu-64bit-build1/ca-certificates.crt
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+	echo "Error occurred in extracting $ARIA2C_BUILD_NAME"
+	exit 21 # terminate and indicate error
+fi
+echo "Extraction completed successfully"
+
+echo "Removing build extract $ARIA2C_BUILD_NAME"
+rm -rf "$ARIA2C_BUILD_NAME"
+echo "Removed build extract $ARIA2C_BUILD_NAME"
+
+
+#converting static binaries to executables
+chmod +x aria2c
+
+#adding them to path variables
+export PATH="$PATH:`pwd`/aria2c"
+echo "aria2c lib installed successfully"
+
+chmod +x Aria2_Static_Build_LINUX-X64.sh
+./Aria2_Static_Build_LINUX-X64.sh
+
+heroku-php-apache2  #start web server
