100f5d8a241961d9622db213b6caa2cc687159a2
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-19 12:15:30 +0530

    Added test code for new download_#05

--- aria2cManager.php
+++ aria2cManager.php
@@ -6,12 +6,21 @@
 		$action = $_POST["action"];
 		
 		switch($action) {
-			case "addDownload": echo "Add download invoked with url, " . $_POST["inputUrl"];
+			case "addDownload": //echo "Add download invoked with url, " . $_POST["inputUrl"];
+								echo addDownload($_POST["inputUrl"]);
 								break;
 			default:	echo "Action not defined";
 		}
 		
 	} else {
 		die("Invalid script invocation");
+	}
+	
+	function addDownload($inputUrl) {
+		$result = $aria2c->addUri(
+			[$inputUrl],
+			['dir'=>'/app/downloads']
+		);
+		return $result["result"];
 	}
 ?>
