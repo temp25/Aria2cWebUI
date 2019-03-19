d64fbd53e7f694e1d12cb0fc605bbd27b179f381
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-19 12:35:30 +0530

    Added test code for new download_#09

--- aria2cManager.php
+++ aria2cManager.php
@@ -1,8 +1,8 @@
 <?php
 	require_once("vendor/autoload.php");
-	$aria2c = new Aria2('http://localhost:6800/jsonrpc');
 	
 	function addDownload($inputUrl) {
+		$aria2c = new Aria2('http://localhost:6800/jsonrpc');
 		$result = $aria2c->addUri(
 			[$inputUrl],
 			['dir'=>'/app/downloads']
@@ -15,12 +15,7 @@
 		
 		switch($action) {
 			case "addDownload": //echo "Add download invoked with url, " . $_POST["inputUrl"];
-								$aria2c_class_methods = get_class_methods($aria2c);
-								$aria2c_class_method_str = "";
-								foreach ($aria2c_class_methods as $method_name) {
-									$aria2c_class_method_str = "$aria2c_class_method_str\n$method_name\n";
-								}
-								echo "Var export : \n" . var_export($aria2c, true) . "\n\n\naria2c_class_method_str : \n\n$aria2c_class_method_str";//addDownload($_POST["inputUrl"]);
+								addDownload($_POST["inputUrl"]);
 								break;
 			default:	echo "Action not defined";
 		}
