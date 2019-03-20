11f4c069e48a0bf537204b7a0d88eb724a67d8c5
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-20 09:01:00 +0530

    Removed conflicts in code

--- aria2cManager.php
+++ aria2cManager.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	require("vendor/autoload.php");
 	
@@ -43,53 +42,4 @@
 	} else {
 		die("Invalid script invocation");
 	}
-	
 ?>
-=======
-<?php
-	require("vendor/autoload.php");
-	
-	function addDownload($aria2c, $inputUrl) {
-		
-		$result = $aria2c->addUri(
-			[$inputUrl],
-			['dir'=>'/app/downloads']
-		);
-		
-		return $result["result"];
-	}
-	
-	function tellStatus($aria2c, $gid) {
-		
-		$result = $aria2c->tellStatus($gid);
-		$status = [];
-		if(array_key_exists("error", $result)) {
-			$status["status"] = "error";
-			$status["message"] = $result["error"]["message"];
-		} else {
-			$status = $result["result"];
-		}
-		return json_encode($status, true);
-	}
-	
-	if(isset($_POST)) {
-		$action = $_POST["action"];
-		$aria2c = new Aria2('http://localhost:6800/jsonrpc');
-		
-		switch($action) {
-			case "addDownload": //echo "Add download invoked with url, " . $_POST["inputUrl"];
-								echo addDownload($aria2c, $_POST["inputUrl"]);
-								break;
-			
-			case "tellStatus": echo tellStatus($aria2c, $_POST["id"]);
-								break;
-								
-			default:	echo "Action not defined";
-		}
-		
-	} else {
-		die("Invalid script invocation");
-	}
-	
-?>
->>>>>>> db9ce577f35af2f1d0bbd48180fae351c568fee2
